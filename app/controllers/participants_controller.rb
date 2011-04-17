43c337f86c02368a0ab0d38ff7b37fa43e997074
Adrian Friedli <adi@koalatux.ch>
2011-04-17 19:16:55 +0200

    license added

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,3 +1,20 @@
+# du7f - a simple web app for simple polls
+# Copyright (C) 2011  Adrian Friedli <adi@koalatux.ch>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 class ParticipantsController < ApplicationController
   before_filter :get_poll_associates, :only => [ :index, :edit ]
   before_filter :get_participant, :only => [ :edit, :update, :destroy ]
