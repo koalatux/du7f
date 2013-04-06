b1fdb7da40f40d99a7d5a4615e365dc184922ba2
Adrian Friedli <adi@koalatux.ch>
2013-04-07 01:00:54 +0200

    changes needed to run on debian wheezy

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,7 @@
 # Be sure to restart your server when you modify this file
 
-require 'thread'
-
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -41,5 +39,10 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 
-  config.action_mailer.default_url_options = { :host => "www.koalatux.ch" }
+  config.action_mailer.default_url_options = { :host => "du7f.koalatux.ch" }
 end
+
+ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => false,
+  :domain => 'du7f.koalatux.ch'
+}
