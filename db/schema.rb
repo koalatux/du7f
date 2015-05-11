4fa77e8d66be60e37b883a7dd7b61df732802c20
Adrian Friedli <adi@koalatux.ch>
2015-05-11 22:44:11 +0200

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,12 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
@@ -22,8 +24,8 @@ ActiveRecord::Schema.define(:version => 20110728082901) do
     t.string   "name"
     t.text     "comment"
     t.integer  "poll_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "comments", ["poll_id"], :name => "index_comments_on_poll_id"
@@ -40,8 +42,8 @@ ActiveRecord::Schema.define(:version => 20110728082901) do
   create_table "participants", :force => true do |t|
     t.string   "name"
     t.integer  "poll_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "participants", ["poll_id"], :name => "index_participants_on_poll_id"
@@ -53,8 +55,8 @@ ActiveRecord::Schema.define(:version => 20110728082901) do
     t.string   "title"
     t.text     "description"
     t.integer  "poll_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
     t.string   "admin_email_address"
     t.boolean  "comments_allowed"
     t.datetime "close_at"
