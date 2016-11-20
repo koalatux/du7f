3b4f81ae4c55553385cda6ecf2a25ef6bce6d657
Adrian Friedli <adi@koalatux.ch>
2016-11-20 15:05:07 +0100

    add new choices via javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,24 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).on('turbolinks:load', function(event) {
+
+// fill and hide input field for detecting dumb spam bots
+$(document).on('turbolinks:load', function (event) {
     $('#poll_calculation, #comment_calculation').val('0x17').closest('.calculation').css('display', 'none');
+});
+
+
+// add new choice to the list
+$(document).on('turbolinks:load', function (event) {
+    $('#add_choice').on('click', function (event) {
+        var choices = $('#choices').children('p');
+        var new_choice = choices.first().clone();
+        var update_index = function (_, str) {
+            return str.replace('0', choices.size());
+        };
+        new_choice.find('label').prop('for', update_index);
+        new_choice.find('input').val('').prop('id', update_index).prop('name', update_index);
+        new_choice.appendTo(choices.parent());
+        return false;
+    });
 });
