11f62d8a3d8d852d3b81126d7470fe63cb81fcc6
Adrian Friedli <adi@koalatux.ch>
2016-11-22 23:00:44 +0100

    more tests

--- test/functional/participants_controller_test.rb
+++ test/functional/participants_controller_test.rb
@@ -5,4 +5,19 @@ class ParticipantsControllerTest < ActionController::TestCase
   #test "the truth" do
   #  assert true
   #end
+
+  test 'should get index' do
+    get :index, token: polls(:alices_poll).token
+    assert_response :success
+    assert_template :index
+
+    # TODO: test participant
+
+    assert assigns(:comment)
+    assert_equal polls(:alices_poll), assigns(:comment).poll
+    assert assigns(:comments)
+    assert assigns(:comments).size >= 2
+    assert assigns(:comments).first.name
+    assert assigns(:comments).first.comment
+  end
 end
