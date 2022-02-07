299b471d0bd61358130fba23e81063ba0cd36427
Adrian Friedli <adi@koalatux.ch>
2022-02-07 22:40:00 +0100

    feat: upgrade to Rails 6.0

--- Gemfile
+++ Gemfile
@@ -2,9 +2,9 @@ source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.0'
+gem 'rails', '~> 6.0.0'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3', '~> 1.3.6'
+gem 'sqlite3', '~> 1.4.0'
 # Use SCSS for stylesheets
 # gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -1,67 +1,61 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110728082901) do
+ActiveRecord::Schema.define(version: 2022_02_07_210433) do
 
-  create_table "choices", :force => true do |t|
-    t.string  "title"
+  create_table "choices", force: :cascade do |t|
+    t.string "title", limit: 255
     t.integer "poll_id"
+    t.index ["poll_id"], name: "index_choices_on_poll_id"
   end
 
-  add_index "choices", ["poll_id"], :name => "index_choices_on_poll_id"
-
-  create_table "comments", :force => true do |t|
-    t.string   "name"
-    t.text     "comment"
-    t.integer  "poll_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "comments", force: :cascade do |t|
+    t.string "name", limit: 255
+    t.text "comment"
+    t.integer "poll_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["poll_id"], name: "index_comments_on_poll_id"
   end
 
-  add_index "comments", ["poll_id"], :name => "index_comments_on_poll_id"
-
-  create_table "entries", :force => true do |t|
+  create_table "entries", force: :cascade do |t|
     t.integer "answer"
     t.integer "participant_id"
     t.integer "choice_id"
+    t.index ["choice_id"], name: "index_entries_on_choice_id"
+    t.index ["participant_id"], name: "index_entries_on_participant_id"
   end
 
-  add_index "entries", ["choice_id"], :name => "index_entries_on_choice_id"
-  add_index "entries", ["participant_id"], :name => "index_entries_on_participant_id"
-
-  create_table "participants", :force => true do |t|
-    t.string   "name"
-    t.integer  "poll_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "participants", force: :cascade do |t|
+    t.string "name", limit: 255
+    t.integer "poll_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["poll_id"], name: "index_participants_on_poll_id"
   end
 
-  add_index "participants", ["poll_id"], :name => "index_participants_on_poll_id"
-
-  create_table "polls", :force => true do |t|
-    t.string   "token"
-    t.string   "admin_token"
-    t.string   "author"
-    t.string   "title"
-    t.text     "description"
-    t.integer  "poll_type"
-    t.datetime "created_at",          :null => false
-    t.datetime "updated_at",          :null => false
-    t.string   "admin_email_address"
-    t.boolean  "comments_allowed"
+  create_table "polls", force: :cascade do |t|
+    t.string "token", limit: 255
+    t.string "admin_token", limit: 255
+    t.string "author", limit: 255
+    t.string "title", limit: 255
+    t.text "description"
+    t.integer "poll_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "admin_email_address", limit: 255
+    t.boolean "comments_allowed"
     t.datetime "close_at"
+    t.index ["token"], name: "index_polls_on_token", unique: true
   end
-
-  add_index "polls", ["token"], :name => "index_polls_on_token", :unique => true
 
 end
